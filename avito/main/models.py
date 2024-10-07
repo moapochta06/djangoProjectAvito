c8c2babae2eec72041519412954114b85a59c559
moapochta06 <90915948+moapochta06@users.noreply.github.com>
2024-10-07 20:16:36 +0700

    добавлено кдаление профиля

---
+++ avito/main/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.dispatch import Signal
+from .utilities import send_activation_notification
+user_registrated = Signal('instance')
+
+
+
+
+class AdvUser(AbstractUser):
+    is_activated = models.BooleanField(default=True,db_index=True, verbose_name='Прошел активацию?')
+    send_message = models.BooleanField(default=True,verbose_name='Оповещать при новых комментариях?')
+
+class Meta(AbstractUser.Meta):
+    pass
+
+def user_registrated_dispatcher(sender, **kwargs):
+   send_activation_notification(kwargs['instance'])
+
+
+user_registrated.connect(user_registrated_dispatcher)
+
+
+
+
+
+
+
+
+
+
+
+
